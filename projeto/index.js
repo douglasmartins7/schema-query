1d5c89bdf35c1ae55f91b4dd3abdd08c7a1cedae
FABIO CONCEICAO ARAUJO <2103965953@corp.nova>
2019-11-19 18:35:34 -0300

    first commit

---
+++ projeto/index.js
@@ -0,0 +1,82 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    # Pontos de entrada  da sua API
+    scalar Date
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    type Query {
+        ola: String! 
+        horaAtual: Date!
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+    }
+
+`
+
+const resolvers = {
+    Produto: {
+        precoComDesconto(produto) {
+            if(produto.desconto) {
+                return produto.preco * (1 - produto.desconto)
+            } else {
+                return produto.preco 
+            }
+        }
+    },
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
+    Query: {
+        ola() {
+            return "Bom dia"
+        },
+        horaAtual() {
+            return `${new Date}`
+        },
+        usuarioLogado(obj) {
+            console.log(obj)
+            return {
+                id: 1,
+                nome: 'Ana da web',
+                email: "anadaweb@email.com",
+                idade: 23,
+                salario_real: 1234.56,
+                vip: true
+            }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: "Notebook Gamer",
+                preco: 4890.89,
+                desconto: 0.5
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
